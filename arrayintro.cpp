e7f35a957fb468dacbbd99e67b79a8b362241edd
Agriya Yadav <agriyayadav@gmail.com>
2025-06-03 10:54:26 +0530

    Worked with two array scripts.

---
+++ arrayintro.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4];
+    cout << "Please enter 4 integers." << endl;
+    
+    for(int i=0; i<4;i++){
+        cin >> arr[i];
+    }
+
+    cout<< "Values in array are now: " << endl;
+
+    for(int i=0; i<4;i++){
+        cout << " " << arr[i]+1;
+    }
+
+    cout << endl;
+    return 0;
+}
